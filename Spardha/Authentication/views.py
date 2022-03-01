c611d1a652c951a6bfec33722cb3021eef7af1bc
Ankur Agrawal <ankuragrawal9455@gmail.com>
2022-03-01 16:49:24 +0530

    Adding authentication

--- Spardha/Authentication/views.py
+++ Spardha/Authentication/views.py
@@ -1,3 +1,249 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, serializers, status
+from rest_framework.response import Response
+from .serializers import (
+    ResetPasswordEmailSerializer,
+    NewPasswordSerializer,
+    RegisterSerializer,
+    check,
+    LoginSerializer,
+    UserSerializer,
+)
+from .models import UserAccount
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import (
+    smart_str,
+    smart_bytes,
+)
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout
+from django.urls import reverse
+from .utils import Util
+from rest_framework.authtoken.models import Token
+from django.shortcuts import redirect
+from django.http import Http404
+from Spardha.settings import BASE_URL_FRONTEND
 
-# Create your views here.
+
+class LoginView(generics.GenericAPIView):
+    """
+    Implement login functionality, taking email and password
+    as input, and returning the Token.
+    """
+
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if username is None or password is None:
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user = authenticate(username=username, password=password)
+        if not user:
+            return Response(
+                {"error": "Please check your credentials...cannot login!"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        elif user.is_active is False:
+            return Response(
+                {"error": "Please verify your email first and then login."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        login(request, user)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+
+
+class LogoutView(generics.GenericAPIView):
+    """
+    TODO:
+    Implement logout functionality, logout the user.
+    """
+
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = LoginSerializer
+
+    def get(self, request):
+        request.user.auth_token.delete()
+        logout(request)
+
+        return Response(status=status.HTTP_200_OK)
+
+
+def create_auth_token(user):
+    """
+    Returns the token required for authentication for a user.
+    """
+    token, _ = Token.objects.get_or_create(user=user)
+    return token
+
+
+class RequestPasswordResetEmail(generics.GenericAPIView):
+    serializer_class = ResetPasswordEmailSerializer
+
+    def post(self, request):
+
+        email = request.data["email"]
+
+        if UserAccount.objects.filter(email=email).exists():
+            user = UserAccount.objects.get(email=email)
+            if not user.is_active:
+                return Response(
+                    {"error": "User not authorized!"},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site = get_current_site(request=request).domain
+            relativeLink = reverse(
+                "password-reset-confirm", kwargs={"uidb64": uidb64, "token": token}
+            )
+            absurl = "http://" + current_site + relativeLink
+            email_body = f"""<h2> Spardha'22 </h2>
+                 <br> <strong> Hello {user.name}! </strong>
+                 <br> We have received a request to reset the password of your Spardha account. <br>
+                 Click the link below to proceed further: <br> <a href='{absurl}'>Reset</a> <br>
+                 If you have any questions, please contact us at 
+                 <a href='mailto:spardha@gmail.com'>spardha@gmail.com</a>"""
+            data = {
+                "email_body": email_body,
+                "to_mail": [user.email],
+                "email_subject": "Reset Your Spardha Password",
+            }
+            Util.send_email(data)
+            return Response(
+                {"success": "Link has been sent by email to reset password"},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"message": "No user with this email id exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+def PasswordTokenCheck(request, uidb64, token):
+    id = smart_str(urlsafe_base64_decode(uidb64))
+    user = UserAccount.objects.get(id=id)
+    if not PasswordResetTokenGenerator().check_token(user, token):
+        raise Http404
+    url = BASE_URL_FRONTEND + "/resetpage?id=" + str(uidb64) + "&token=" + str(token)
+    return redirect(url)
+
+
+class NewPasswordView(generics.GenericAPIView):
+    serializer_class = NewPasswordSerializer
+
+    def patch(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(
+            {"success": True, "message": "Password reset successful"},
+            status=status.HTTP_200_OK,
+        )
+
+
+class UserUpdateView(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = UserAccount.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        try:
+            user = request.user
+            content = {
+                "name": user.name,
+                "email": user.email,
+                "institution": user.institution_name,
+                "designation": user.designation,
+                "phone": user.phone_no,
+            }
+            return Response(content, status=status.HTTP_200_OK)
+        except serializers.get_error_detail:
+            return Response(
+                {"error": "An error occurred!"}, status=status.HTTP_403_FORBIDDEN
+            )
+
+    def post(self, request):
+        user = UserAccount.objects.filter(email=request.user.email)
+        if user is not None:
+            if "name" in request.data:
+                user.update(name=request.data["name"])
+            if "designation" in request.data:
+                user.update(
+                    year=request.data["designation"],
+                )
+            if "institution_name" in request.data:
+                user.update(
+                    institution_name=request.data["institution_name"],
+                )
+            return Response(
+                {"message": "Updated successfully!"}, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {"error": "An error occurred!"}, status=status.HTTP_403_FORBIDDEN
+            )
+
+
+class RegisterView(generics.GenericAPIView):
+    queryset = UserAccount.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = check(request.data)
+            if user is None:
+                user = serializer.save()
+                create_auth_token(user=user)
+                uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+                token = PasswordResetTokenGenerator().make_token(user)
+                current_site = get_current_site(request=request).domain
+                relativeLink = reverse(
+                    "activate-account", kwargs={"uidb64": uidb64, "token": token}
+                )
+                absurl = "http://" + current_site + relativeLink
+                email_body = f"""<h2> Spardha'22 </h2>
+                     <br> <strong> Hello {user.name}! </strong>
+                     <br> Thanks for registering on Spardha <br>
+                     To complete your sign up, we just need to verify your email address.<br>
+                     Click the link below to verify: <br> <a href='{absurl}'>Verify</a> <br>
+                     <br> If you have any questions, please contact us at 
+                     <a href='mailto:spardha@gmail.com'>spardha@gmail.com</a>"""
+                data = {
+                    "email_body": email_body,
+                    "to_mail": [user.email],
+                    "email_subject": "Activate Your Spardha Account",
+                }
+                Util.send_email(data)
+                return Response(
+                    {"success": "Verification link has been sent by email!"},
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    {"error": "User with same credentials already exists!"},
+                    status=status.HTTP_226_IM_USED,
+                )
+        else:
+            error = {}
+            for err in serializer.errors:
+                error[err] = serializer.errors[err][0]
+            return Response(error, status=status.HTTP_409_CONFLICT)
+
+
+def ActivateAccount(request, uidb64, token):
+    id = smart_str(urlsafe_base64_decode(uidb64))
+    user = UserAccount.objects.get(id=id)
+    if not PasswordResetTokenGenerator().check_token(user, token):
+        raise Http404
+    url = BASE_URL_FRONTEND + "/login"
+    user.is_active = True
+    user.save()
+    return redirect(url)
--- Spardha/Spardha/urls.py
+++ Spardha/Spardha/urls.py
@@ -1,21 +1,49 @@
-"""Spardha URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = "Spardha Site Backend Administration"
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Spardha Site API",
+        default_version="v1",
+        description="This is the Spardha Site API.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("auth/", include("Authentication.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    # swagger API pages not visible on production
+    urlpatterns += [
+        path(
+            "",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "auth/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "user/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        ),
+    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
