77262443d4609f2c6a7260d7506ec0a41f2825a8
atharvabhatt200 <atharvabhatt200@gmail.com>
2022-03-02 12:41:16 +0530

    add models for Game, Team and Player

--- Spardha/Teams/admin.py
+++ Spardha/Teams/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Game, Team, Player
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(Team)
+admin.site.register(Player)
--- Spardha/Teams/models.py
+++ Spardha/Teams/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from Authentication.models import UserAccount
 
-# Create your models here.
+
+class Game(models.Model):
+    gameName = models.CharField(max_length=20, unique=True)
+    min_players = models.IntegerField()
+    max_players = models.IntegerField()
+
+    def __str__(self):
+        return self.gameName
+
+
+class Team(models.Model):
+    college_rep = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    num_of_players = models.IntegerField()
+
+    def __str__(self):
+        return self.game.gameName + self.college_rep.institution_name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    is_captain = models.BooleanField()
+
+    def __str__(self):
+        return self.name + ("_Captain" if self.is_captain else "")
